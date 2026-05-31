49a23eb535da99b6fa46ce1037744f4ba0b38f38
samuel19-14 <carvalhosamuel1914@gmail.com>
2026-05-31 12:05:51 -0300

    quarto commit do JS

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ botoesGorjeta.forEach(botao => {
     botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento) {
+function receberPorcentagem(evento) {
     botoesGorjeta.forEach(botao =>{
         botao.classLista.remove(botao-ativo)
 
@@ -40,7 +40,15 @@ function receberPorcentagemBotao(evento) {
         }
     })
 
-    
-    porcentagem = parseFloat(evento.target.value) / 100
+    if(evento.target.value !== ""){
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
+    }    
+
 }
+
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
 
